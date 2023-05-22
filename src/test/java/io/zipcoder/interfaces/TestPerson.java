c7750d99d2dc8a6c7f43fc9f4be3dbf8cda3f6ae
Miamivice2 <andreb.davis2@gmail.com>
2023-05-22 15:18:34 -0400

    created person test

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
 public class TestPerson {
+    public static void main(String[] args) {
+        testConstructor();
+        testSetName();
+    }
 
+    public static void testConstructor() {
+        Person person = new Person(1L, "John Doe");
+        System.out.println("ID: " + person.getId());
+        System.out.println("Name: " + person.getName());
+    }
+
+    public static void testSetName() {
+        Person person = new Person(1L, "John Doe");
+        System.out.println("Before setting name: " + person.getName());
+
+        person.setName("Jane Smith");
+        System.out.println("After setting name: " + person.getName());
+    }
 }
+
